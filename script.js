4eabd62269aefa9e9e59139bae28a55f8c384694
nahlalimbrick1 <nahla.limbrick@aspirestudent.org>
2020-01-29 02:29:04 +0000

    i finally completed a project

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Write your click handler and conditionals here
+$("button").click(function() {
+    let Food=$("#Food").val();
+    let age=$("#age").val();
+    if (age>30 && Food==="pasta" ){
+        $("#answer-header").text("Ann Perkins");
+    } else if (age<30 && Food==="pizza") {
+        $("#answer-header").text("Ron Swanson");
+    }else if (age>30 && Food==="pizza") {
+        $("#answer-header").text("April Ludgate"); 
+    }else if (age<30 && Food==="pasta") {
+        $("#answer-header").text("Leslie Knopp")
+    }
+});
